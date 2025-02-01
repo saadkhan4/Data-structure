2f2218bc9ce28542a99e6d01bc46736ac5504921
saadkhan4 <saadshaheen1327@gmail.com>
2025-02-01 21:43:45 +0500

    practice of different functions

--- Leet_Code/twosums.py
+++ Leet_Code/twosums.py
@@ -1,25 +1,25 @@
-nums = [2,7,11,15]
-target = 9
+# nums = [2,7,11,15]
+# target = 9
 
 
-def two_sums(nums, target):
-    seen = {}
-    for i in range(len(nums)):
-        diff = target - nums[i]    
-        if diff in seen:
-            return [seen[diff],i]   # Agar diff pehle se dictionary mein hai, to yeh uske index (jo seen[diff] mein store hai) aur current index i ko return kar dega.
-        else:
-            seen[nums[i]] = i      # Agar diff dictionary mein nahi milta, to hum current number nums[i] ko dictionary mein store kar lete hain aur uska index i uske saath rakhte hain.
+# def two_sums(nums, target):
+#     seen = {}
+#     for i in range(len(nums)):
+#         diff = target - nums[i]    
+#         if diff in seen:
+#             return [seen[diff],i]   # Agar diff pehle se dictionary mein hai, to yeh uske index (jo seen[diff] mein store hai) aur current index i ko return kar dega.
+#         else:
+#             seen[nums[i]] = i      # Agar diff dictionary mein nahi milta, to hum current number nums[i] ko dictionary mein store kar lete hain aur uska index i uske saath rakhte hain.
 
 
-result = two_sums(nums, target)
-print(result)
+# result = two_sums(nums, target)
+# print(result)
         
 
-nums = [4,8,9,2]
-target = 11
+nums = [5,2,3,1]
+target = 7
 
-def two_sums(nums,target):
+def two_sums(nums, target):
     seen = {}
     for i in range(len(nums)):
         diff = target - nums[i]
@@ -28,7 +28,6 @@ def two_sums(nums,target):
         else:
             seen[nums[i]] = i
 
-
-result = two_sums(nums,target)
-print(result)            
+result = two_sums(nums , target)
+print(result)
 
